7499841a67572989e83ec683d192ebb082c75922
4ngell4S <4senc1os@gmail.com>
2024-01-09 11:22:59 -0500

    tipo de datos set

---
+++ Tipos_de_datos/8-sets.py
@@ -0,0 +1,23 @@
+#los sets son tipo de datos mutables y no ordenada
+
+#declarando un set
+mi_set = {1,2,3,4,5}
+print(mi_set)
+
+# los sets no pueden tener elementos repetidos
+mi_set = set()
+mi_set.add(1)
+mi_set.add(2)
+mi_set.add(2)
+mi_set.add(3)
+print(mi_set)
+
+#usando set para eliminar elementos repetidos de una lista
+lista_numero = [1,1,3,4,6,6,1,7]
+mi_set = set(lista_numero)
+print(mi_set)
+
+pertenece = 10 in mi_set
+print(pertenece)
+
+frutas = {'manzana', 'Banana', 'pera', 'uva'}
