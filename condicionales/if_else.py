aa3c12e67092a967de1f8b110cf96a5737d512ab
4ngell4S <4senc1os@gmail.com>
2024-01-13 23:13:47 -0500

    if else encadenado

--- condicionales/if_else.py
+++ condicionales/if_else.py
@@ -1,8 +1,21 @@
 # condicionales una forma de controlar el flujo de un programa
 
-edad = 17
+# edad = 17
 
-if edad >= 18:
-   print("Es mayor de edad")
+# if edad >= 18:
+#    print("Es mayor de edad")
+# else:
+#    print("Es menor de edad")
+
+
+# if else encadenado
+edad = 60
+
+if edad >= 0 and edad <= 12:
+   print("Usted es un niño")
+elif edad >= 13 and edad <= 17:
+   print("Usted es un adolescente")
+elif edad >= 18 and edad <= 59:
+   print("Usted es un adulto")
 else:
-   print("Es menor de edad")
+   print("Usted es un adulto mayor")
