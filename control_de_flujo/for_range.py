a132a697f77f76b0c9b2db8c95450d796dfe6709
4ngell4S <4senc1os@gmail.com>
2024-01-16 12:00:26 -0500

    for utilizando range

---
+++ control_de_flujo/for_range.py
@@ -0,0 +1,5 @@
+# la varialble _ da a entender que esa variable no se esta utilizando
+# range(4) genera una lista de 0 a 3, esto nos genera 4 iteraciones e imprime 4 veces el mensaje
+for _ in range(4):
+   print("desde origen avanzar 10 pasos")
+   print("girar 90 grados")
