8c49a9745d97b8c7d6e2ad4a334316178f08973d
4ngell4S <4senc1os@gmail.com>
2024-01-23 16:31:54 -0500

    parametros

---
+++ funciones/parametros.py
@@ -0,0 +1,20 @@
+def sumar(numero1, numero2):
+   resultado = numero1 + numero2
+   print(f"La suma entre {numero1} y {numero2} es {resultado}")
+
+def restar(numero1, numero2):
+   resultado = numero1 - numero2
+   print(f"La resta entre {numero1} y {numero2} es {resultado}")
+
+def multiplicar(numero1, numero2):
+   resultado = numero1 * numero2
+   print(f"La multiplicación entre {numero1} y {numero2} es {resultado}")
+
+def dividir(numero1, numero2):
+   resultado = numero1 / numero2
+   print(f"La división entre {numero1} y {numero2} es {resultado}")
+
+sumar(10, 20)
+restar(10, 20)
+multiplicar(10, 20)
+dividir(10, 20)
