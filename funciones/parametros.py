761d77fdc3c19a5bae997b6cc4086346d3b36e23
4ngell4S <4senc1os@gmail.com>
2024-01-23 16:39:40 -0500

    ejercicio practico funciones operaciones aritmeticas

--- funciones/parametros.py
+++ funciones/parametros.py
@@ -1,20 +1,30 @@
 def sumar(numero1, numero2):
    resultado = numero1 + numero2
-   print(f"La suma entre {numero1} y {numero2} es {resultado}")
+   print(f"La suma de {numero1} y {numero2} es {resultado}")
 
 def restar(numero1, numero2):
    resultado = numero1 - numero2
-   print(f"La resta entre {numero1} y {numero2} es {resultado}")
+   print(f"La resta de {numero1} y {numero2} es {resultado}")
 
 def multiplicar(numero1, numero2):
    resultado = numero1 * numero2
-   print(f"La multiplicación entre {numero1} y {numero2} es {resultado}")
+   print(f"La multiplicación de {numero1} y {numero2} es {resultado}")
 
 def dividir(numero1, numero2):
    resultado = numero1 / numero2
-   print(f"La división entre {numero1} y {numero2} es {resultado}")
+   print(f"La división de {numero1} y {numero2} es {resultado}")
 
-sumar(10, 20)
-restar(10, 20)
-multiplicar(10, 20)
-dividir(10, 20)
+operacion = input("Que operación quieres realizar? (sumar, restar, multiplicar, dividir): ")
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+if operacion == "sumar":
+   sumar(num1, num2)
+elif operacion == "restar":
+   restar(num1, num2)
+elif operacion == "multiplicar":
+   multiplicar(num1, num2)
+elif operacion == "dividir":
+   dividir(num1, num2)
+else:
+   print("Operación no válida, Escriba sumar,"\
+      " restar, multiplicar o dividir")
