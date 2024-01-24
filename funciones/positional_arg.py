41a590ca2a059e29c543bd5877961dc4e6b0cea5
4ngell4S <4senc1os@gmail.com>
2024-01-23 21:19:15 -0500

    pisitional arguments

---
+++ funciones/positional_arg.py
@@ -0,0 +1,12 @@
+# Argumentos posicionales -> son los argumentos que se pasan en el orden en el que se declaran en la función
+
+def imprimir_nombre(primer_nombre,
+                    segundo_nombre,
+                    primer_apellido,
+                    segundo_apellido):
+   
+   print(f"Hola {primer_nombre} {segundo_nombre} "\
+      f"{primer_apellido} {segundo_apellido} "\
+      "bienvenido al curso de python")   
+
+imprimir_nombre("Angel", "Julian", "Asencios", "Mory")
