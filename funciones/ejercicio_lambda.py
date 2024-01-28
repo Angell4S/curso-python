46deda703fe44fbdbe880e6da19cf7595286b8f9
4ngell4S <4senc1os@gmail.com>
2024-01-27 23:57:14 -0500

    practicando lambda comparando numeros y potencia

--- funciones/ejercicio_lambda.py
+++ funciones/ejercicio_lambda.py
@@ -16,3 +16,11 @@ print(sumar(10,11))
 print(restar(10,11))
 print(multi(10,11))
 print(dividir(10,11))
+
+# lambda x: x > 2 -> x es el parametro que recibe la funcion lambda, x > 2 es la condicion que se va a evaluar, (3) es el valor que se le pasa a la funcion lambda
+mayor_menor = (lambda x: x > 2)(3)
+print(mayor_menor) # => True
+
+# lambda x, y: x ** 2 + y ** 2 -> x, y son los parametros que recibe la funcion lambda, x ** 2 + y ** 2 es la operacion que se va a realizar, (2, 1) son los valores que se le pasan a la funcion lambda
+potencia = (lambda x, y: x ** 2 + y ** 2)(2, 1)
+print(potencia) # => 5
