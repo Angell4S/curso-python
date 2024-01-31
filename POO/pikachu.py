3967cf15b42f4aeeca05836b75ec3ec406de5044
4ngell4S <4senc1os@gmail.com>
2024-01-31 11:04:34 -0500

    antes de encapsulamiento

--- POO/pikachu.py
+++ POO/pikachu.py
@@ -1,10 +1,14 @@
 class Pikachu:
+   
    tipo = "Eléctrico"
 
-   def __init__(self, nombre, nivel, salud):
+   def __init__(self, nombre, nivel, salud, voltaje_maximo, amperaje_max, color):
       self.nombre = nombre
       self.nivel = nivel
       self.salud = salud
+      self.voltaje_maximo = voltaje_maximo
+      self.amperaje_max = amperaje_max
+      self.color = color
 
    def atacar(self):
       print(f"¡Pikachu ataca y genera {self.nivel/4} de daño!")
@@ -12,12 +16,10 @@ class Pikachu:
 
 
 
-pikachu_1 = Pikachu("Pepe", 750, 500)
-pikachu_2 = Pikachu("Rocko", 1200, 1000)
+pikachu_1 = Pikachu("Pepe", 750, 500, 6, 2, "amarillo")
+print(f"El pikachu llamado {pikachu_1.nombre} tiene un nivel {pikachu_1.nivel} y es de tipo {pikachu_1.tipo}.")
 
-print(pikachu_1.nombre, pikachu_1.nivel, pikachu_1.salud, pikachu_1.tipo)
-print(f"El pikachu llamado {pikachu_1.nombre} ataca.")
-pikachu_1.atacar()
+pikachu_1.nivel = -900
+pikachu_1.tipo = "Agua"
 
-print(f"El pikachu llamado {pikachu_2.nombre} ataca.")
-pikachu_2.atacar()
+print(f"El pikachu llamado {pikachu_1.nombre} tiene un nivel {pikachu_1.nivel} y es de tipo {pikachu_1.tipo}.")
