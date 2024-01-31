ca155f6918204c7a50df0e9161340555de29a1d1
4ngell4S <4senc1os@gmail.com>
2024-01-31 18:45:47 -0500

    encapsulamiento privado y publico

--- POO/pikachu.py
+++ POO/pikachu.py
@@ -1,17 +1,18 @@
 class Pikachu:
    
-   tipo = "Eléctrico"
+# _ significa que es privado, pero se puede acceder y modificar, __ significa que es privado y no se puede acceder ni modificar
+   __tipo = "Eléctrico"
 
    def __init__(self, nombre, nivel, salud, voltaje_maximo, amperaje_max, color):
       self.nombre = nombre
-      self.nivel = nivel
-      self.salud = salud
-      self.voltaje_maximo = voltaje_maximo
-      self.amperaje_max = amperaje_max
+      self.__nivel = nivel
+      self.__salud = salud
+      self.__voltaje_maximo = voltaje_maximo
+      self.__amperaje_max = amperaje_max
       self.color = color
 
    def atacar(self):
-      print(f"¡Pikachu ataca y genera {self.nivel/4} de daño!")
+      print(f"¡Pikachu ataca y genera {self.__nivel/4} de daño!")
 
 
 
@@ -19,7 +20,7 @@ class Pikachu:
 pikachu_1 = Pikachu("Pepe", 750, 500, 6, 2, "amarillo")
 print(f"El pikachu llamado {pikachu_1.nombre} tiene un nivel {pikachu_1.nivel} y es de tipo {pikachu_1.tipo}.")
 
-pikachu_1.nivel = -900
-pikachu_1.tipo = "Agua"
+pikachu_1.__nivel = -900
+pikachu_1.__tipo = "Agua" 
 
-print(f"El pikachu llamado {pikachu_1.nombre} tiene un nivel {pikachu_1.nivel} y es de tipo {pikachu_1.tipo}.")
+print(f"El pikachu llamado {pikachu_1.nombre} tiene un nivel {pikachu_1.nivel} y es de tipo {pikachu_1.tipo}, su voltaje maximo es {pikachu_1.__voltaje_maximo}.")
