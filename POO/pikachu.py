6763082d6fcffed42ee88559c4c7db4098d7da34
4ngell4S <4senc1os@gmail.com>
2024-01-30 22:28:43 -0500

    aplicando metodos

--- POO/pikachu.py
+++ POO/pikachu.py
@@ -1,13 +1,23 @@
 class Pikachu:
-    tipo = "Eléctrico"
+   tipo = "Eléctrico"
 
-    def __init__(self, nombre, nivel, salud):
-        self.nombre = nombre
-        self.nivel = nivel
-        self.salud = salud
+   def __init__(self, nombre, nivel, salud):
+      self.nombre = nombre
+      self.nivel = nivel
+      self.salud = salud
+
+   def atacar(self):
+      print(f"¡Pikachu ataca y genera {self.nivel/4} de daño!")
+
+
 
 
 pikachu_1 = Pikachu("Pepe", 750, 500)
 pikachu_2 = Pikachu("Rocko", 1200, 1000)
+
 print(pikachu_1.nombre, pikachu_1.nivel, pikachu_1.salud, pikachu_1.tipo)
-print(pikachu_2.nombre, pikachu_2.nivel, pikachu_2.salud, pikachu_2.tipo)
+print(f"El pikachu llamado {pikachu_1.nombre} ataca.")
+pikachu_1.atacar()
+
+print(f"El pikachu llamado {pikachu_2.nombre} ataca.")
+pikachu_2.atacar()
