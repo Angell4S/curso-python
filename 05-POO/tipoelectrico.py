bdd673145139f8adf895ae687fd2b836cc3bc77c
Angell4S <4senc1os@gmail.com>
2024-03-25 11:21:28 -0500

    linea o jerarquia de herencia

---
+++ 05-POO/tipoelectrico.py
@@ -0,0 +1,37 @@
+
+from pokemon import Pokemon # Importamos la clase pokemon del archivo pokemon.py
+
+class TipoElectrico(Pokemon):
+
+   __tipo = "Electrico"
+
+   def __init__(self, nombre, nivel, salud, voltaje_max, amperaje_max, color):
+      super().__init__(nombre=nombre, nivel=nivel, salud=salud, color=color)
+      self.__voltaje_maximo = None
+      self.__amperaje_maximo = None
+
+      self.voltaje_maximo = voltaje_max
+      self.amperaje_maximo = amperaje_max
+   
+
+   @property
+   def voltaje_maximo(self):
+      return self.__voltaje_maximo
+
+   @voltaje_maximo.setter
+   def voltaje_maximo(self, voltaje_maximo):
+      if voltaje_maximo > 0:
+         self.__voltaje_maximo = voltaje_maximo
+      else:
+         print("El voltaje no puede ser negativo.")
+   
+   @property
+   def amperaje_maximo(self):
+      return self.__amperaje_maximo
+   
+   @amperaje_maximo.setter
+   def amperaje_maximo(self, amperaje_maximo):
+      if amperaje_maximo > 0:
+         self.__amperaje_maximo = amperaje_maximo
+      else:
+         print("El amperaje no puede ser negativo.")
