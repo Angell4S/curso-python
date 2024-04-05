5bad83e70ed5f9a084cf539e8b1967e080209016
4ngell4S <4senc1os@gmail.com>
2024-04-05 10:55:55 -0500

    inicio de proyeto sistema solar, abstraccion

---
+++ 06-practica-POO/start.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Star:
+   
+   def __init__(self, image_path, mass, nucleo_status):
+      self.image_path = image_path
+      self.mass = mass
+      self.nucleo_status = nucleo_status
